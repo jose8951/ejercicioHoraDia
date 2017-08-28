d710f5e9af3e3ea3cfebd55f875b119180fc7e99
javega09 <javega09@hotmail.com>
2017-08-28 19:16:34 +0200

    ejercicio 1 termiando

--- src/ejerciciohoradia/EjercicioHoraDia.java
+++ src/ejerciciohoradia/EjercicioHoraDia.java
@@ -5,7 +5,7 @@
 package ejerciciohoradia;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+
 import java.io.InputStreamReader;
 
 public class EjercicioHoraDia {
@@ -16,38 +16,39 @@ public class EjercicioHoraDia {
      */
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
+        System.out.println("introducce la hora");
         int horas = Integer.parseInt(br.readLine());
+        System.out.println("introducce los minutos");
         int minutos = Integer.parseInt(br.readLine());
-        
+
         /**
-         * 0 a 7 madrugada
-         * 7 a 12 Mañana
-         * 12 a 21 Tarde
-         * 21 a 0 Noche
+         * 0 a 7 madrugada 7 a 12 Mañana 12 a 21 Tarde 21 a 0 Noche
          */
-        
-        if (horas ==12 && minutos==0)
+        if (horas == 12 && minutos == 0) {
             System.out.println("mediodia");
-        else
-            if(horas==0 && minutos==0)
+        } else {
+            if (horas == 0 && minutos == 0) {
                 System.out.println("medianoche");
-        else
-                if(horas>=0 && horas<=7)
+            } else {
+                if (horas >= 0 && horas <= 7) {
                     System.out.println("madrugada");
-        else
-                    if(horas>7 && horas<=12)
+                } else {
+                    if (horas > 7 && horas <= 12) {
                         System.out.println("Mañana");
-        else
-                        if(horas >12 && horas <=21)
+                    } else {
+                        if (horas > 12 && horas <= 21) {
                             System.out.println("tarde");
-        else
-                            if(horas >21 && horas<=23)
+                        } else {
+                            if (horas > 21 && horas <= 23) {
                                 System.out.println("noche");
-        
-                                
-                
-        
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
-    
+
 }
